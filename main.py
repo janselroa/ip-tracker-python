0f2cc5115068782ee0aee9a9124c565c305f775c
jansel <janselroa2424@gmail.com>
2022-02-06 15:18:10 -0400

    add preview

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from os import system
 option = input("Quiere ver los datos de su direccion ip o de un dominio externo(PC/WEB)\n\n").lower()
 
 api_key=""
@@ -7,14 +8,16 @@ with open("api_key.txt","r") as file:
 url=f"https://geo.ipify.org/api/v2/country,city?apiKey={api_key}"
 
 def show_data(data):
+   system("cls")
    location = data['location']
    time_zone = location['timezone'].replace("-","")
    print(f"IP: {data['ip']}\nname: {data['as']['name']}")
-   print(f"location: {location['region']}, {location['country']}")
+   print(f"location:{location['city']}-{location['region']}, {location['country']}")
    print(f"Hora: {time_zone}")
    print(f"Proveedor de internet: {data['isp']}")
 
 if option == "pc":
+   print("loading...")
    ip = requests.get("https://api.ipify.org/?format=json").json()['ip']
    response = requests.get(f"{url}&ipAddress={ip}")
    user_data = response.json()
@@ -23,5 +26,8 @@ if option == "pc":
 elif option == "web":
    address = input("Ingrese la direccion del sitio web:\n").lower()
    response = requests.get(f"{url}&domain={address}")
+   print("loading...")
    data = response.json()
    show_data(data)
+else:
+    print("No has especificado bien la opcion")
